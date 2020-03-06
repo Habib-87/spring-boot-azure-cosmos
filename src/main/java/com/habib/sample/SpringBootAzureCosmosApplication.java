a80ba9a592a2ffe3bc30603e7d003f191e1eeff9
Habib <habib87@gmail.com>
2020-03-06 14:01:32 +0100

    Initial Commit

---
+++ src/main/java/com/habib/sample/SpringBootAzureCosmosApplication.java
@@ -0,0 +1,13 @@
+package com.habib.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureCosmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureCosmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habib/sample/SpringBootAzureCosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habib.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureCosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
